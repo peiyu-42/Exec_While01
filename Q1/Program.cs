027f00e87766db2e5aaeb9bcc259db43752498e8
User <User@DESKTOP-49HMLNI>
2022-10-23 16:37:42 +0800

    加入專案檔案。

---
+++ Q1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q1
+{
+	internal class Program
+	{
+		/* 1+ 2 + 3 + 4 + .... + N <105
+		 * 請問 N 是多少 ?*/
+		static void Main(string[] args)
+		{
+			WhileSum result = new WhileSum();
+			int n = result.SumToN(105);
+			Console.WriteLine($"N= {n}");
+		}
+		
+	}
+	class WhileSum
+	{
+		public int SumToN(int number)
+		{
+			// 若1加到最大值n小於number，則傳回n
+			int sum = 1;
+			int n = 1;
+			while (sum < number)
+			{
+				n++;
+				sum += n;
+			}
+			if (sum >= number)
+			{ n--; }
+			return n;
+		}
+	}
+}
---
+++ Q2/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q2
+{
+	/* 使用 while , 在畫面顯示所有可以整除 300 的整數
+	 * 例如: 1, 2, 3, 4, 5, 6, 10, ...., 300
+	 */
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			int number = 300;
+			WhileForDivision result = new WhileForDivision();
+			Console.WriteLine($"所有可以整除 {number} 的整數");
+			result.DivisorNumber(number);
+		}
+	}
+	class WhileForDivision
+	{
+		public void DivisorNumber(int number) 
+		{
+			int divisor = 1; // 除數
+			while (divisor <= number)
+			{
+				if (number % divisor == 0)
+				{
+					Console.WriteLine(divisor);
+				}
+				divisor++;
+			}
+		}
+	}
+
+}
